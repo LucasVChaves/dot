48687bb9a6b0c7f35ae4147c5900d61d085560bc
Lucas Chaves <lucasvchaves@gmail.com>
2024-11-07 11:08:26 -0300

    Wezterm opens in full screen as default

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,12 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+-- Set fullscreen on startup
+wezterm.on('gui-startup', function (cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
 
 -- Lua table that hold the configurations
 local config = wezterm.config_builder()
@@ -14,6 +21,12 @@ config.font_size = 18
 
 -- ColorScheme
 config.color_scheme = 'Gruvbox Dark (Gogh)'
+
+-- Cursor
+config.default_cursor_style = "BlinkingBlock"
+config.cursor_blink_rate = 750
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
 
 -- Return modified config table
 return config
